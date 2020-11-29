384067a6b5538256d67d2b8bcfbb79af60341243
Matthew Damron <mattdamron@msn.com>
2020-11-29 13:43:20 -0600

    add hours of the day

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,32 +1,76 @@
 // pull and set current date form moment
 $('#currentDay').text(moment().format('MMMM Do YYYY'));
 
-// generating textareas for scheduling
-for (var hour = 9; hour < 18; hour++) {
-    // scheduledHours.push(moment({hour}).format('h  a'));
-    var taskDiv = $('<div>')
-        .addClass('row');
-    
-    var taskHour = $('<div>')
-        .addClass('col time-block')
-        .text(moment({hour}).format('hA'));
-    
-    var taskDescription = $('<textarea>')
-        .addClass('col-10 description past')
-        .attr('id', 'description');
+var currentHour = function (taskDescription, hour) {
+    // var taskDescription = $('<textarea>');
 
-    var taskSaveIcon = $('<i>')
-        .addClass('far fa-save');
+    if (moment().hour() > hour) {
+        taskDescription
+            .addClass('past')
+            .removeClass('present')
+            .removeClass('future');
+    }
+    else if (moment().hour() === hour) {
+        taskDescription
+            .removeClass('past')
+            .addClass('present')
+            .removeClass('future');
+    }
+    else {
+        taskDescription
+            .removeClass('past')
+            .removeClass('present')
+            .addClass('future');
+    }
 
-    var taskSave = $('<button>')
-        .addClass('col saveBtn')
-        .append(taskSaveIcon);
-
-    taskDiv
-        .append(taskHour)
-        .append(taskDescription)
-        .append(taskSave);
-    
-    $('.container')
-        .append(taskDiv);
+    return taskDescription;
 }
+
+var setHoursOfDay = function() {
+// generating textareas for scheduling
+    for (var hour = 9; hour < 18; hour++) {
+        // scheduledHours.push(moment({hour}).format('h  a'));
+        var taskDiv = $('<div>')
+            .addClass('row');
+        
+        var taskHourSpan = $('<span>')
+            // .addClass('mx-auto')
+            .text(moment({hour}).format('hA'));
+        
+        var taskHourDiv = $('<div>')
+            .addClass('col text-dark time-block')
+            // .attr('hour', hour)
+            // .append(taskHourSpan);
+            .text(moment({hour}).format('hA'));
+        
+        var taskDescription = $('<textarea>')
+            .addClass('col-9 text-dark description')
+            .attr('id', 'description')
+            .attr('hour', hour);
+            
+            currentHour(taskDescription, hour);
+
+        var taskSaveIcon = $('<i>')
+            .addClass('far fa-save');
+
+        var taskSave = $('<button>')
+            .addClass('col saveBtn')
+            .append(taskSaveIcon);
+
+        taskDiv
+            .append(taskHourDiv)
+            .append(taskDescription)
+            .append(taskSave);
+        
+        $('.container')
+            .append(taskDiv);
+    };
+};
+
+setHoursOfDay();
+
+setInterval(function () {
+    $(".card .list-group-item").each(function(index, el) {
+      auditTask(el);
+    });
+  }, (1000 * 60) * 15);
