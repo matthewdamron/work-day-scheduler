6b976da784ebd4bc4a135cdef11e5de34166b25b
Matt Damron <mattdamron@msn.com>
2020-12-01 11:50:19 -0700

    add the ability to change start and end hours

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,8 +1,45 @@
 // set array for hourly tasks
 var hourlyTasks = [];
 
+// setup selector for start time
+var startHourSelect = $('#startHour');
+var hourlyTasksStart = JSON.parse(localStorage.getItem("hourlyTasksStart"));
+if (!hourlyTasksStart) {
+    // set default time to 9
+    startHourSelect.val('09:00');
+    localStorage.setItem("hourlyTasksStart", JSON.stringify(startHourSelect.val()));
+    hourlyTasksStart = JSON.parse(localStorage.getItem("hourlyTasksStart"));
+}
+else {
+    startHourSelect.val(hourlyTasksStart);
+}
+
+// setup selector for end time
+var endHourSelect = $('#endHour');
+var hourlyTasksEnd = JSON.parse(localStorage.getItem("hourlyTasksEnd"));
+if (!hourlyTasksEnd) {
+    // set default time to 17
+    endHourSelect.val('17:00');
+    localStorage.setItem("hourlyTasksEnd", JSON.stringify(endHourSelect.val()));
+    hourlyTasksEnd= JSON.parse(localStorage.getItem("hourlyTasksEnd"));
+}
+else {
+    endHourSelect.val(hourlyTasksEnd);
+}
+
 // pull and display current date from moment
 $('#currentDay').text(moment().format('MMMM Do YYYY'));
+
+// option to change start and end hours of the day.
+var changeStartStopTime = function() {
+    if (startHourSelect.val() > endHourSelect.val() || endHourSelect.val() < startHourSelect.val()) {
+        alert('Start and End time cant be larger or smaller then each other! Typical work hours are 9AM to 5PM.')
+    }
+    else {
+        localStorage.setItem("hourlyTasksStart", JSON.stringify(startHourSelect.val()));
+        localStorage.setItem("hourlyTasksEnd", JSON.stringify(endHourSelect.val()));
+    }
+};
 
 // set description color based on the hour of the day
 var currentHour = function (taskDescription, hour) {
@@ -35,7 +72,7 @@ var currentHour = function (taskDescription, hour) {
 // function to create hour time blocks
 var setHoursOfDay = function() {
     // set hours of the day from 9 to 5
-    for (var hour = 9; hour < 18; hour++) {
+    for (var hour = hourlyTasksStart.substring(0, 2); hour <= hourlyTasksEnd.substring(0, 2); hour++) {
         // create <div> for hour block
         var taskDiv = $('<div>')
             .addClass('row m-1');
@@ -138,6 +175,18 @@ $('.saveBtn').on('click', function() {
         saveTasks();
     }    
 });
+
+// $('#startHour').change('change', function() {
+    // localStorage.setItem("hourlyTasksStart", JSON.stringify(startHourSelect.val()));
+    // var startHourSplitVal = startHourSelect.val().split(":");
+    // var startHourSplit = startHourSplitVal[0];
+    // console.log('test');
+// });
+
+// $('#endHour').change('change', function() {
+    // localStorage.setItem("hourlyTasksEnd", JSON.stringify(endHourSelect.val()));
+    // console.log('test');
+// });
 
 // set time interval to reload the page every 15 min
 setInterval(function () {
